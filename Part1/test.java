947a74d99c3d959c7b4cb7277012de3f6622afd8
LimJieYing <limjieying234@gmail.com>
2024-04-23 20:22:23 +0100

    Added updated test file with instructions and comments

--- Part1/test.java
+++ Part1/test.java
@@ -1,14 +1,22 @@
 public class test {
     public static void main(String[] args) {
-        Race race = new Race(30, "m" ,2 );
+
+        //create a new Race object
+        Race race = new Race(10, "m" ,4 );
+
+        //create three Horse objects
         Horse horse1 = new Horse('+', "Frank", 0.7);
         Horse horse2 = new Horse('*', "Bob", 0.5);
+        Horse horse3 = new Horse('%', "cOW", 0.3);
         
         race.addHorse(horse1, 1);
         race.addHorse(horse2, 2);
+        //the third lane is left empty
+        race.addHorse(horse3, 4);
         
-       
+        //Start the race
         race.startRace();
+        //to start the race multiple times, use race.startRace(); multiple times
 
     }
 }
